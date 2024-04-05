2e17e262b6f5c3b841f930e7c582706ae4af2dc5
Siva Sankar Reddy <aasamsivasankarreddynani143@gmail.com>
2024-04-05 22:10:12 +0530

    cors

--- frontend/src/redux/api/apiSlice.js
+++ frontend/src/redux/api/apiSlice.js
@@ -1,34 +1,34 @@
-// import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
-
-// // These are utility functions provided by Redux Toolkit for creating an API interface and performing network requests
-
-// import { BASE_URL } from "../constants";
-
-// const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL }); //base URL that will be used for all API requests.
-
-// export const apiSlice = createApi({
-//   baseQuery,
-//   endpoints: () => ({}),
-// });
-
 import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
+
+// These are utility functions provided by Redux Toolkit for creating an API interface and performing network requests
+
 import { BASE_URL } from "../constants";
 
-const baseQuery = fetchBaseQuery({
-  baseUrl: BASE_URL,
-  prepareHeaders: (headers, { getState }) => {
-    // Include credentials to allow cookies to be sent
-    headers.set("Content-Type", "application/json");
-    headers.set("Accept", "application/json");
-    headers.set(
-      "Access-Control-Allow-Origin",
-      "https://movies-database-seven.vercel.app"
-    );
-    return headers;
-  },
-});
+const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL }); //base URL that will be used for all API requests.
 
 export const apiSlice = createApi({
   baseQuery,
   endpoints: () => ({}),
 });
+
+// import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
+// import { BASE_URL } from "../constants";
+
+// const baseQuery = fetchBaseQuery({
+//   baseUrl: BASE_URL,
+//   prepareHeaders: (headers, { getState }) => {
+//     // Include credentials to allow cookies to be sent
+//     headers.set("Content-Type", "application/json");
+//     headers.set("Accept", "application/json");
+//     headers.set(
+//       "Access-Control-Allow-Origin",
+//       "https://movies-database-seven.vercel.app"
+//     );
+//     return headers;
+//   },
+// });
+
+// export const apiSlice = createApi({
+//   baseQuery,
+//   endpoints: () => ({}),
+// });
