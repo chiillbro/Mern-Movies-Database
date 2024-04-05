f7f77eb7ca563e309914f7362e8783db1584976d
Siva Sankar Reddy <aasamsivasankarreddynani143@gmail.com>
2024-04-05 21:32:17 +0530

    cmmi

--- backend/index.js
+++ backend/index.js
@@ -46,15 +46,12 @@ app.use("/api/v1/movies", moviesRoutes);
 
 app.use("/api/v1/upload", uploadRoutes);
 
-const __dirname = path.resolve(); // Node.js method that resolves the full path of the current module file
+// const __dirname = path.resolve(); // Node.js method that resolves the full path of the current module file
 
 // express.static() is Express middleware to serve static files such as images, CSS files, and JavaScript files.
 
 // configuring Express to serve static files located in the "uploads" directory at the "/uploads" URL path
-app.use(
-  "/backend/uploads",
-  express.static(path.join(__dirname + "/backend/uploads"))
-);
+app.use("/uploads", express.static(path.join(__dirname + "uploads")));
 
 app.listen(port, () => console.log(`Listening to Server : ${port}`));
 
