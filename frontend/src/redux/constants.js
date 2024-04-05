0138c2c63af684a2b757efb71e22fdeee4577880
Siva Sankar Reddy <aasamsivasankarreddynani143@gmail.com>
2024-04-05 20:27:00 +0530

    xx commit

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,9 +1,13 @@
-export const BASE_URL = "";
+export const BASE_URL = "https://movies-database-seven.vercel.app/";
 
-export const USERS_URL = "/api/v1/users";
+export const USERS_URL =
+  "https://movies-database-seven.vercel.app//api/v1/users";
 
-export const GENRE_URL = "/api/v1/genre";
+export const GENRE_URL =
+  "https://movies-database-seven.vercel.app//api/v1/genre";
 
-export const MOVIE_URL = "/api/v1/movies";
+export const MOVIE_URL =
+  "https://movies-database-seven.vercel.app//api/v1/movies";
 
-export const UPLOAD_URL = "/api/v1/upload";
+export const UPLOAD_URL =
+  "https://movies-database-seven.vercel.app//api/v1/upload";
